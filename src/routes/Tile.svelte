dc7f71a45b589da4e6513449de285b085360d00c
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 12:18:15 +0000

    Improve project tile look

--- src/routes/Tile.svelte
+++ src/routes/Tile.svelte
@@ -1,9 +1,11 @@
 <script>
 	import github from '$lib/images/github.svg';
+	import link_img from '$lib/images/link.svg';
 
 	export let title = "Title";
 	export let summary = "A short summary of this tile.";
 	export let link = "";
+	export let source = "";
 </script>
 
 <section class="tile">
@@ -18,6 +20,11 @@
 	<footer>
 		{#if link != ""}
 		<a href={link}>
+			<img src={link_img} alt="link to project" height="20vh" />
+		</a>
+		{/if}
+		{#if source != ""}
+		<a href={source}>
 			<img src={github} alt="GitHub" height="20vh" />
 		</a>
 		{/if}
@@ -29,5 +36,8 @@
 	background-color: rgba(0, 0, 0, 0.1);
 	padding: 32px;
 	border-radius: 8px;
+}
+footer {
+	margin: 8px;
 }
 </style>
