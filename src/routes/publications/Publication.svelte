f71fd8399b41bf6fb890bd74a5874a866d0da4f8
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 12:17:42 +0000

    Improve publication look

--- src/routes/publications/Publication.svelte
+++ src/routes/publications/Publication.svelte
@@ -1,9 +1,11 @@
 <script>
 	import github from '$lib/images/github.svg';
+	import link_img from '$lib/images/link.svg';
 
 	export let title = "Title";
 	export let summary = "A short summary of this tile.";
 	export let link = "";
+	export let publication = "";
 </script>
 
 <section class="tile">
@@ -13,12 +15,21 @@
 		</h1>
 	</header>
 
-	{summary}
+	<details>
+		<summary>Read more...</summary>
+		<p>{summary}</p>
+	</details>
 
 	<footer>
 		{#if link != ""}
 		<a href={link}>
 			<img src={github} alt="GitHub" height="20vh" />
+		</a>
+		{/if}
+		{#if publication != ""}
+		<a href={publication}>
+			paper
+			<img src={link_img} alt="publication link" height="20vh" />
 		</a>
 		{/if}
 	</footer>
@@ -29,5 +40,8 @@
 	background-color: rgba(0, 0, 0, 0.1);
 	padding: 32px;
 	border-radius: 8px;
+}
+footer {
+	margin: 8px;
 }
 </style>
