1527de2810c61c3bd584d5cba0a4abbfea75b3e5
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 13:46:44 +0000

    Populate publications list from a file, improve look

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,29 +1,22 @@
 <script>
 	import Publication from './Publication.svelte';
+	import { publications } from '../publications.js';
 </script>
 
 <main>
 <h1>Publications</h1>
 
-<h2>2022</h2>
 
 <div class="projects">
-	<Publication
-		title="Integrating simulation tools and Electron Microscopy"
-		authors="Beton, Joseph; Cragnolini, Tristan; Kaleel, Manaz; Mulvaney, Thomas; Sweeney, Aaron; Topf, Maya;"
-	/>
-	<Publication
-		title="Cryo-EM Structure and B-Factor Refinement with Ensemble Representation"
-		authors="Cragnolini, T.; Beton, J.; Topf, M."
-	/>
-	<Publication
-		title="Conformational sampling of protein structure prediction"
-		summary="We develop statistical models to improve the sampling of alternative conformational states in protein structure prediction pipelines."
-	/>
-	<Publication
-		title="Flexible fitting for cryo-EM"
-		summary="We develop statistical models to represent the relation between structural variations in biomolecular complexes and the observed cryo-EM density."
-	/>
+	{#each publications as publication}
+		<Publication
+			title={publication.Title}
+			authors={publication.authors}
+			summary={publication["Abstract Note"]}
+			publication={publication.Url}
+			DOI={publication.DOI}
+		/>
+	{/each}
 </div>
 
 </main>
@@ -31,7 +24,7 @@
 <style>
 .projects {
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(600px, 1fr));
 	gap: 32px;
 }
 </style>
--- src/routes/publications/Publication.svelte
+++ src/routes/publications/Publication.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { blur, fade, fly, slide } from 'svelte/transition';
+
 	import github from '$lib/images/github.svg';
 	import link_img from '$lib/images/link.svg';
 
@@ -7,28 +9,39 @@
 	export let summary = "A short summary of this tile.";
 	export let link = "";
 	export let publication = "";
+	export let DOI = "";
+	let open = false;
 </script>
 
 <section class="tile">
 	<header>
-		<h1>
+		<h3>
 			{title}
-		</h1>
+		</h3>
 		<p>{authors}</p>
 	</header>
 
-	<details>
-		<summary>Read more...</summary>
-		<p>{summary}</p>
+	{#if summary}
+	<details bind:open>
+		<summary>Abstract</summary>
+		{#if open}
+			<p transition:slide={{ duration: 1000 }} >{summary}</p>
+		{/if}
 	</details>
+	{/if}
 
 	<footer>
-		{#if link != ""}
+		{#if DOI}
+		<a href=https://doi.org/{DOI}>
+			DOI: {DOI}
+		</a>
+		{/if}
+		{#if link}
 		<a href={link}>
 			<img src={github} alt="GitHub" height="20vh" />
 		</a>
 		{/if}
-		{#if publication != ""}
+		{#if publication}
 		<a href={publication}>
 			paper
 			<img src={link_img} alt="publication link" height="20vh" />
@@ -38,15 +51,30 @@
 </section>
 
 <style>
+details {
+	margin: 20px;
+	margin: 20px;
+}
+summary {
+	color: var(--color-theme-1);
+}
 .tile {
 	background-color: rgba(0, 0, 0, 0.1);
-	padding: 32px;
+	padding: 12px;
+	margin: 12px;
 	border-radius: 8px;
+	position: relative;
 }
 .tile header {
 	display: inline;
 }
-footer {
-	margin: 8px;
+.tile footer {
+	position: absolute;
+	padding: 5px;
+	margin: 5px;
+	bottom: 0;
+}
+footer a {
+margin: 8px;
 }
 </style>
