b6e467f5eaa333b3a8770d80405d59547baf5a94
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 15:01:24 +0000

    Updates

---
+++ _app/immutable/nodes/2.9619994a.js
@@ -0,0 +1,7 @@
+import{s,n as a}from"../chunks/scheduler.845e7f00.js";import{S as r,i as l,g as m,h as c,x as h,a as d,f}from"../chunks/index.2636905a.js";function p(o){let e,n=`I am a researcher in computational biohysics, and bioinformatics.
+My work has centered on how biomolecules fold, and the effect their folding has on their properties.
+I have worked on the folding of DNA, and proteins, bioinformatics analysis of large sequence datasets, and the analysis and simulation of SELEX experiments.
+
+<br/> <br/>
+
+Find my group <a href="https://cragnolini-lab.github.io">website here</a>.`;return{c(){e=m("main"),e.innerHTML=n},l(t){e=c(t,"MAIN",{"data-svelte-h":!0}),h(e)!=="svelte-1edszno"&&(e.innerHTML=n)},m(t,i){d(t,e,i)},p:a,i:a,o:a,d(t){t&&f(e)}}}class b extends r{constructor(e){super(),l(this,e,null,p,s,{})}}export{b as component};
