2268c2e5e043f4f2f32392973350fce8f6aa77e9
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 00:44:15 +0000

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+
+I am a researcher in computational biohysics, and bioinformatics.
+My work so far has centered on how RNA molecules fold, and the effect their folding has on their properties.
+I have touched on other related topics: the folding of DNA, and proteins, bioinformatics analysis of large sequence datasets, and the analysis and simulation of SELEX experiments.
+Some works have been mostly independent: participating in the development of a normal mode analysis webserver for proteins (webnma), a protein pocket identification software.
+
+<br />
+<br />
+
+Find my group <a href="https://cragnolini-lab.github.io">website here</a>.
+
+</main>
