a47953301e31256ecf5257ca9eed56a1d084805d
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2023-12-07 13:48:08 +0000

    Improve front page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <main>
 
 I am a researcher in computational biohysics, and bioinformatics.
-My work so far has centered on how RNA molecules fold, and the effect their folding has on their properties.
-I have touched on other related topics: the folding of DNA, and proteins, bioinformatics analysis of large sequence datasets, and the analysis and simulation of SELEX experiments.
-Some works have been mostly independent: participating in the development of a normal mode analysis webserver for proteins (webnma), a protein pocket identification software.
+My work has centered on how biomolecules fold, and the effect their folding has on their properties.
+I have worked on the folding of DNA, and proteins, bioinformatics analysis of large sequence datasets, and the analysis and simulation of SELEX experiments.
 
 <br />
 <br />
