b2d6c8e1daeb58d5bc79a8c178544dd9b3615771
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2024-03-27 20:17:32 +0000

    Add longer descriptions for research axes

--- src/routes/research/+page.svelte
+++ src/routes/research/+page.svelte
@@ -8,15 +8,15 @@
 <div class="projects">
 	<Tile
 		title="Understand dynamic ensembles of biomolecular assemblies"
-		summary="Use methods that inherently represent ensembles of models. Use them to understand molecular flexibility and dynamics."
+		summary=" To understand biomolecular assemblies, we need to represent the ensemble of conformations they go through to perform their function, from folding, to enzymatic mechanisms. This require methods that inherently represent these ensembles. I develop probabillistic models and methods, that can handle a distribution of conformations, and use them to understand molecular flexibility and dynamics."
 	/>
 	<Tile
 		title="Cellular-scale molecular dynamics"
-		summary="Develop novel methods to tackle large assemblies. Automated, scaling up of simulations to the meso-scale."
+		summary="To understand the biophysics of cellular assemblies, new tools are required. I develop methods that automate the process of scaling up simulations, enabling the representations of process at the mesoscale."
 	/>
 	<Tile
 		title="Integrated biophysical modelling: end-to-end representations"
-		summary="Integrate representation of biophysical experiment in data modelling."
+		summary="To interpret biophysical data, a detailed understanding of the process that produce this data is paramount. My work focus on developing end-to-end representations of biophysical experiments, to accurately understand the data they produce."
 	/>
 </div>
 
