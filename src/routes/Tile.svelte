39ba9bbe56af730f5d8d1a3df4ccc6ee5b43342a
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2024-03-27 21:40:41 +0000

    Improve research page, add links to projects

--- src/routes/Tile.svelte
+++ src/routes/Tile.svelte
@@ -1,11 +1,16 @@
 <script>
+	import { blur, fade, fly, slide } from 'svelte/transition';
+
 	import Github from './Github.svelte';
 	import Link from './Link.svelte';
 
 	export let title = "Title";
 	export let summary = "A short summary of this tile.";
+	export let details;
 	export let link = "";
 	export let source = "";
+
+	let open = false;
 </script>
 
 <section class="tile">
@@ -15,7 +20,21 @@
 		</h1>
 	</header>
 
+	{#if details}
+	<details bind:open>
+		<summary>{summary}</summary>
+		{#if open}
+		<ul transition:slide={{ duration: 1000 }} >
+			{#each details as det}
+			<li>{det}</li>
+			{/each}
+		</ul>
+		{/if}
+	</details>
+	{:else}
 	{summary}
+	{/if}
+
 
 	<footer>
 		{#if link}
--- src/routes/research/+page.svelte
+++ src/routes/research/+page.svelte
@@ -9,14 +9,20 @@
 	<Tile
 		title="Understand dynamic ensembles of biomolecular assemblies"
 		summary=" To understand biomolecular assemblies, we need to represent the ensemble of conformations they go through to perform their function, from folding, to enzymatic mechanisms. This require methods that inherently represent these ensembles. I develop probabillistic models and methods, that can handle a distribution of conformations, and use them to understand molecular flexibility and dynamics."
+		details={["Contact prediction with deep learning in flexible proteins",
+			 "Conformational sampling of protein structure prediction"]}
 	/>
 	<Tile
 		title="Cellular-scale molecular dynamics"
 		summary="To understand the biophysics of cellular assemblies, new tools are required. I develop methods that automate the process of scaling up simulations, enabling the representations of process at the mesoscale."
+		details={["Hierarchical coarse-grain modelling",
+			 "Mycobacterium cell wall"]}
 	/>
 	<Tile
 		title="Integrated biophysical modelling: end-to-end representations"
 		summary="To interpret biophysical data, a detailed understanding of the process that produce this data is paramount. My work focus on developing end-to-end representations of biophysical experiments, to accurately understand the data they produce."
+		details={["Flexible fitting for cryo-EM",
+			 "Statistical interpretation of XL-MS data"]}
 	/>
 </div>
 
