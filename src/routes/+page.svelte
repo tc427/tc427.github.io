e7e0f28dc9f6eda97d7366c3bae9e441636507c7
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2024-03-27 17:05:16 +0000

    Add link to projects on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <main>
 
 I am a researcher in computational biohysics, and bioinformatics.
-My work has centered on how biomolecules fold, and the effect their folding has on their properties.
+My work centres on how biomolecules fold, and the effect their folding has on their properties.
+
+<h2><a href="./research">Current projects</a></h2>
+<ul>
+	<li> Understand dynamic ensembles of biomolecular assemblies </li>
+	<li> Cellular-scale molecular dynamics </li>
+	<li> Integrated biophysical modelling: end-to-end representations </li>
+</ul>
 I have worked on the folding of DNA, and proteins, bioinformatics analysis of large sequence datasets, and the analysis and simulation of SELEX experiments.
 
 <br />
