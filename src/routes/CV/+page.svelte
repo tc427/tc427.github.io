f5d3ff6868af4e99522230dbd64e5a930fb04e64
Tristan Cragnolini <tristan.cragnolini@gmail.com>
2024-03-27 17:25:17 +0000

    Improve CV page: positions, authors in publications

--- src/routes/CV/+page.svelte
+++ src/routes/CV/+page.svelte
@@ -8,6 +8,10 @@
     "2008-2009 Exchange student at San José State University, San José, California, USA",
     "2005-2009 Bachelor in chemistry and biology, Université Paris 12, Créteil, France",
 	];
+	const positions = [
+    "2018-2022 Postdoc in the Topf and Thalassinos groups , Birkbeck & UCL, University of London, London",
+    "2014-2016 Wales group , University of Cambridge, Cambridge"
+	];
 </script>
 
 <main>
@@ -27,8 +31,17 @@
 <ul>
 	{#each publications as publication}
 		<li>
-			{publication.Title}
-			doi: <a href="http://doi.org/{publication.DOI}">{publication.DOI}</a>
+			{publication.authors}, "{publication.Title}"
+			<a href="http://doi.org/{publication.DOI}">doi: {publication.DOI}</a>
+		</li>
+	{/each}
+</ul>
+
+<h3>Positions</h3>
+<ul>
+	{#each positions as pos}
+		<li>
+			{pos}
 		</li>
 	{/each}
 </ul>
